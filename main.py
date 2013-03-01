4985ec81ccbbc1f90adcd675a1794a82760c714a
osowiecki-lukasz <lukaszosowiecki@hotmail.com>
2013-03-01 13:44:45 +0100

    Deleted more lines

--- main.py
+++ main.py
@@ -2,21 +2,7 @@ def main():
     print "nie podam"
     print "Nowa linijka"
     print ":D :P"
-    for x in range(5):
-        print "jashdjah"
-        print "Piotrek"
-        print 'linijka', x
-    print "inny tekst"
-    print "qwerty300000000"
-    print "Arturro:-)"
-    print "szp: wtf?/aaaasss"
-	print "dodatkowe"
-    print "Sztembus22:-)"
-	print "sugasugasuga"
-    print "szp: wtf?"
-    print "mergetest"
-    print "oooooooo"
-
+	print "jaoisdj "
 if __name__ == "__main__":
 print "asdf"
 print "asdf3"
