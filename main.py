c6387272e53adeb5909cfbc5e5141509dcdf430e
michal666 <michal.ruszczyk89@gmail.com>
2013-03-01 12:27:46 +0100

    am

--- main.py
+++ main.py
@@ -6,16 +6,13 @@ def main():
         print "Piotrek"
         print 'linijka', x
     print "inny tekst"
-<<<<<<< HEAD
     print "szp: wtf?/aaaasss"
 	print "dodatkowe"
     print "Sztembus:-)"
 
-=======
     print "szp: wtf?"
     print "mergetest"
     print "oooooooo"
-    
->>>>>>> 79d6702d21673fe37aa4aed1f88dd2fa0cd84f59
+
 if __name__ == "__main__":
     main()
