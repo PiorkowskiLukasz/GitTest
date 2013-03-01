47fc84c1ee7647a9015410c385d1765abbfc96b6
Dariusz Wasiulewski <darek.byq360@gmail.com>
2013-03-01 12:16:02 +0100

    zmiana tekstu

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     print "testowa klasa"
     print ":D :P"
     for x in range(5):
-        print "minka"
+        print "jashdjah"
         print 'linijka', x
     print "inny tekst"
     print "szp: wtf?/aaaasss"
