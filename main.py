71a713eab10b7c8529704d38acc67fd57cb4a322
Arturro3 <artcik@oe2.pl>
2013-03-01 12:33:43 +0100

    zmieniony printf na masterze

--- main.py
+++ main.py
@@ -7,18 +7,15 @@ def main():
         print "Piotrek"
         print 'linijka', x
     print "inny tekst"
-<<<<<<< HEAD
     print "qwerty300000000"
     print "Arturro:-)"
-=======
     print "szp: wtf?/aaaasss"
 	print "dodatkowe"
     print "Sztembus:-)"
-
+	print "sugasuga"
     print "szp: wtf?"
     print "mergetest"
     print "oooooooo"
->>>>>>> e52adf0161fcd79b3ff7afffbec6e20e5cbfaf58
 
 if __name__ == "__main__":
 print "asdf"
