27da849ea05d56804066d05f7e0a6f6aa8b6548f
Damian Krogul <ala@niepodam.pl>
2013-03-01 15:06:14 +0100

    fixy

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 def main():
     print "nie podam"
     print "aaaaaaaa"
-    print "Nowa linijka"
+    print "qqqqqqqqqqqqqq"
     print "Pytoo rzadzi i tyle"
     print ":( :("
     for x in range(5):
