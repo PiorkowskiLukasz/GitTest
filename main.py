d22e443ac54a0c6bb5f7282705bbf4d7b2d31553
Nyam-Ochir <maarek.gerwel@gmail.com>
2013-03-01 13:38:05 +0100

    I fixed your code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
     print "testowa klasa"
     print "inny tekst"
+    print "I totally didn't make any changes, if you're so unconvinced you can check my status in github.. it would either say I did some changes which is not true, or it will say I didn't make changes, which I didn't"
     
 if __name__ == "__main__":
     main()
