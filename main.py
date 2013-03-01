d9b31a36b5604d2a0d5b9c07ba6630f40cdad32f
Admin <aaa@uwm.edu.pl>
2013-03-01 13:38:09 +0100

    added few lines

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 def main():
     print "testowa klasa"
+    x = 2
     print ":D :P"
     for x in range(5):
         print "jashdjah"
-        print "Piotrek"
+        print "roman"
         print 'linijka', x
     print "inny tekst"
     print "szp: wtf?"
-    print "mergetest"
     
 if __name__ == "__main__":
     main()
