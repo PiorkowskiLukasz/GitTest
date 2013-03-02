b5382d1f4b608fba46e24c768092c1c8ca7fe22a
Szydlowski <mianek2107@wp.pl>
2013-03-02 12:45:59 +0100

    koment

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():...
         print "Piotrek"
         print 'linijka', x
     print "inny tekst"
-    print "qwerty300000000"
+    print "123456"
     print "Arturro:-)"
     print "szp: wtf?/aaaasss"
     print "DK"
