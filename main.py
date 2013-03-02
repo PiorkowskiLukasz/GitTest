aacd6f02ed45441ebd673a61fbe5b058392b43b5
desyn <desyn.masielo@gmail.com>
2013-03-02 12:44:20 +0100

    dodanie i przy piotrek

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main():...
     print "dfgadfga"
     for x in range(5):
         print "jashdjah"
-        print "Piotrek"
+        print "Piotreki"
         print 'linijka', x
     print "inny tekst"
     print "qwerty300000000"
