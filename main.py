116d03f4f92a01b8a01598589d36f518aefefbca
platoszmichal <michaelxxxx@o2.pl>
2013-03-02 12:53:18 +0100

    zmianaaaa

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():...
         print "Piotrek"
         print 'linijka', x
     print "inny tekst"
-    print "qwerty3000000004444444"
+    print "qwerty30000000043333333333333444444"
     print "Arturro:-)"
     print "szp: wtf?/aaaasss"
     print "DK"
