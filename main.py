2646fba74038357689fea415e7b05746245850b2
Dariusz Wasiulewski <grochu_185@wp.pl>
2013-03-02 12:53:20 +0100

    test

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():...
         print "Piotrek"
         print 'linijka', x
     print "inny tekst"
-    print "qwerty444"
+    print "qwerty666"
     print "Arturro:-)"
     print "szp: wtf?/aaaasss"
     print "DK"
