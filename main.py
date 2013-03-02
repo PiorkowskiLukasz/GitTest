72d979bfd9f922d8daa88338c7e3444b03431cdb
Admin <admin@uwm.pl>
2013-03-02 13:58:15 +0100

    usuniety syf

--- main.py
+++ main.py
@@ -2,18 +2,6 @@
 def main():...
     print "nie podam"
     print "dfgadfga"
-    for x in range(5):
-        print "jashdjah"
-        print "Piotreki"
-        print 'linijka', x
-<<<<<<< HEAD
-    print "inny tekscior"
-    print "qwerty300000000"
-=======
-    print "inny tekst"
-    print "qwerthhhy444469696969696969444444"
->>>>>>> de9846f891dab22232a2bdc0c022a8dfb9cbfa78
->>>>>>> e87fd92a1089ca4170b337ffa9a82c831c5feb9b
     print "Arturro:-)"
     print "szp: wtf?/aaaaxxx"
     print "DK"
