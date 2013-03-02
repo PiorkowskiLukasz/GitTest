0e9f7c2e2c7b85dff1d76762badbabb5e35fb137
Admin <admin@uwm.pl>
2013-03-02 12:34:13 +0100

    poprawki

--- main.py
+++ main.py
@@ -1,35 +1,6 @@
 # -*- coding: cp1250 -*-
-def main():
-    a = 32154;
-def main():kgbyhvjhk
-def main():...
-def main():kgbyhvjhk
 def main():...
     print "nie podam"
-    print "aaaaaaaa"
-<<<<<<< HEAD
-<<<<<<< HEAD
-    print "Nbnhbhjbjh"
-    print "aaaaaaaafghfg"
-=======
-<<<<<<< HEAD
-=======
->>>>>>> d2fefc9560ca0cb4e7e19d766b0e4f50ea6a4218
-    print "Nowa linijka"
-	print "Kuraa lubi placki "
-    print "Kura rzadzi i tyle"
-	print "KUuuuuuuuura"
-    print "costam"
-    print "nowe"
-	print "qqqqqqqqqqqqqq"
-    print "qwertyuioop"
-<<<<<<< HEAD
->>>>>>> e65f9c8c5606db79144fa0ca43766e69ce289084
->>>>>>> 7797ca97872885140863629cb68bedcd6dadf31a
-=======
->>>>>>> d2fefc9560ca0cb4e7e19d766b0e4f50ea6a4218
-    print "Pytoo rzadzi i tyle"
-	print ":( -.-"
     print "dfgadfga"
     for x in range(5):
         print "jashdjah"
